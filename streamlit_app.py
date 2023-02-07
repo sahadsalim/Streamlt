b0231a78ba0d06797176ab2f4d033c26bfb8c3e2
Sahad S <64737914+sahadsalim@users.noreply.github.com>
2023-02-07 12:50:57 +0530

    file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-
-data = pd.read_csv("csv_practice.csv") #path folder of the data file
-st.write(data) #displays the table of data
+st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+# data = pd.read_csv("csv_practice.csv") #path folder of the data file
+# st.write(data) #displays the table of data
