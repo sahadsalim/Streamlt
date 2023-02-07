3f6e52fd06a3b581373d7bf78cf1aad7d014caa2
Sahad S <64737914+sahadsalim@users.noreply.github.com>
2023-02-07 13:10:34 +0000

    updated schema

--- code.py
+++ code.py
@@ -1,14 +1,22 @@
 import pandas as pd;
 y=[{
-    "color": "purple",
-    "type": "minivan",
-    "capacity": 7
+    "symbol": "green",
+    "trade_type": "but",
+    "quantity": 100,
+    "price":"25
   },
   {
-    "color": "red",
-    "type": "station wagon",
-    "capacity": 5
-  }]
+    "symbol": "red",
+    "trade_type": "sell",
+    "quantity": 100,
+    "price":"25
+  },
+{
+    "symbol": "green",
+    "trade_type": "sell",
+    "quantity": 100,
+    "price":"35
+  },]
 x=pd.DataFrame(y)
 z=pd.Series(y)
 # print(x)
@@ -20,7 +28,7 @@ stack=list();
 for dd in z:
     print(dd)
     if(len(stack)>0):
-        c=[x for x in stack if x['capacity'] == 5]
+        c=[x for x in stack if x['symbol'] == dd['symbol']]
         # c=stack.find(car => car['capacity'] === 5);
         if len(c) == 0 :
             stack.append(dd)
