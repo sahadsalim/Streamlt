85b0ff945f0e29a287f9cb97ea0230687fc5510b
sahadsha <sahads9745@gmail.com>
2023-02-11 22:35:26 +0530

    Finding the distance between two lists of geographic coordinates

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,60 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import sklearn.neighbors
+
 st.write("homeee")
-df=st.session_state['df']
-st.write(df)
+# Sidebar setup
+st.sidebar.title('Sidebar')
+with st.sidebar:
+    upload_file = st.sidebar.file_uploader('Upload a file containing earthquake data')
+
+    # Check if file has been uploaded
+    if upload_file is not None:
+        df = pd.read_csv(upload_file)
+        st.session_state['df'] = df;
+    for key in st.session_state.keys():
+        if key=='df':
+            df=st.session_state['df'];
+            lf=df.head();
+            st.write(lf);
+# Create two dataframes with city names and lat-long in degrees
+locations_A = pd.DataFrame({
+    'city_A' :     ['Atlanta', 'Aspen', 'Albuquerque', 'Ann Arbor'],
+    'latitude_A':  [ 33.75,     39.19,   35.08,         42.28],
+    'longitude_A': [ -84.39,    -106.82, -106.65,       -83.74]
+})
+locations_B = pd.DataFrame({
+    'city_B':      ['Boston', 'Baltimore', 'Berkley', 'Bellevue'],
+    'latitude_B' : [ 42.36,    39.29,       37.87,     47.61],
+    'longitude_B': [ -71.06,   -76.61,      -122.27,   -122.20]
+})
+
+# add columns with radians for latitude and longitude
+locations_A[['lat_radians_A','long_radians_A']] = (
+    np.radians(locations_A.loc[:,['latitude_A','longitude_A']])
+)
+locations_B[['lat_radians_B','long_radians_B']] = (
+    np.radians(locations_B.loc[:,['latitude_B','longitude_B']])
+)
+
+
+dist = sklearn.neighbors.DistanceMetric.get_metric('haversine')
+dist_matrix = (dist.pairwise
+    (locations_A[['lat_radians_A','long_radians_A']],
+     locations_B[['lat_radians_B','long_radians_B']])*3959
+)
+# Note that 3959 is the radius of the earth in miles
+df_dist_matrix = (
+    pd.DataFrame(dist_matrix,index=locations_A['city_A'], 
+                 columns=locations_B['city_B'])
+)
+
+
+# Unpivot this dataframe from wide format to long format.
+# When you unpivot, the data in the pivot table becomes a
+# column named 'value'. Rename this column to 'miles' for clarity.
+df_dist_long = (
+    pd.melt(df_dist_matrix.reset_index(),id_vars='city_A')
+)
+df_dist_long = df_dist_long.rename(columns={'value':'miles'})
