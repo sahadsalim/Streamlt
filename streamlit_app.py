11f75dc7bc214f3aa92c36e236ad6c13e49930e8
sahadsha <sahads9745@gmail.com>
2023-02-11 21:00:58 +0530

    removed page three

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,7 +157,7 @@ def page2():
 page_names_to_funcs = {
     "Main Page": main_page,
     "Page 2": page2,
-    "Page 3": page3,
+    # "Page 3": page3,
 }
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
