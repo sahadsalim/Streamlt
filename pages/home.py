47be6d362e86da569dc6f1a2c88f32272122a1ff
sahadsha <sahads9745@gmail.com>
2023-02-12 11:35:23 +0530

    das

--- pages/home.py
+++ pages/home.py
@@ -81,7 +81,7 @@ newdf = pd.DataFrame({
 st.write(newdf)
 newdf['Distance']=[distance(newdf.iloc[i]) for i in range(len(newdf))]
 st.write(":heavy_minus_sign:" * 74)
-st.text("Updated with :blue[distance]")
+st.subheader("Updated with :blue[distance] ")
 # distanceDf=newdf
 # # distanceDf = distanceDf.assign(Product=lambda x: (x['Latitude'] * x['Longitude']))
 # st.write(distanceDf)
