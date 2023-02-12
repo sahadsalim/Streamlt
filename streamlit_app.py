65ffcf30a7a4e125c4947f0c2a8ce5fb049f75bb
sahadsha <sahads9745@gmail.com>
2023-02-12 12:20:19 +0530

    yt

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,19 +30,20 @@ def main_page():
             st.write("Please choose the tradebook csv file")
             uploaded_file = st.file_uploader("Choose a file")
             if uploaded_file is not None or not dataframe.empty:
-                # To read file as bytes:
-                bytes_data = uploaded_file.getvalue()
-            #     st.write(bytes_data)
+                if uploaded_file:
+                    # To read file as bytes:
+                    bytes_data = uploaded_file.getvalue()
+                #     st.write(bytes_data)
 
-                # To convert to a string based IO:
-                stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            #     st.write(stringio)
+                    # To convert to a string based IO:
+                    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+                #     st.write(stringio)
 
-                # To read file as string:
-                string_data = stringio.read()
-            #     st.write(string_data)
+                    # To read file as string:
+                    string_data = stringio.read()
+                #     st.write(string_data)
 
-                # Can be used wherever a "file-like" object is accepted:
+                    # Can be used wherever a "file-like" object is accepted:
                 dataframe = pd.read_csv(uploaded_file)
                 st.write(dataframe)
 
